f1aaaa8517f05495478da678646095ff0b154d94
Cost6527 <clarice.coliveira@yahoo.com.br>
2020-10-22 15:23:32 -0300

    Add new feature 'create new campground'

--- Project 10 - YelpCamp/app.js
+++ Project 10 - YelpCamp/app.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+let campgrounds = [
+    { name: "Salmon Creek", image: "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350" },
+    { name: "Granite Hill", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350" },
+    { name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350" }
+]
 
 app.get("/", function (req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-    let campgrounds = [
-        { name: "Salmon Creek", image: "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350" },
-        { name: "Granite Hill", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350" },
-        { name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350" }
-    ]
     res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function (req, res) {
+    // get data from form and add to campgrounds array
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCampground = { name: name, image: image }
+    campgrounds.push(newCampground);
+    // redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function (req, res) {
+    res.render("new");
 });
 
 app.listen(8080, function () {
